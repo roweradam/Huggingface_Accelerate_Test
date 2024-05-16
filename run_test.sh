bbfbc59686b3dc662e32c04dee9775b62eeb500e
roweradam <148457445+roweradam@users.noreply.github.com>
2024-05-16 13:31:43 +0100

    Create run_test.sh
    Vary number of gpus and method of training (python and accelerate)

---
+++ run_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+## Request GPU partitions
+#SBATCH -p gpu
+## Request the number of GPUs to be used (if more than 1 GPU is required, change 1 into Ngpu, where Ngpu=2,3,4)
+#SBATCH --gres=gpu:1
+## Request the number of nodes
+#SBATCH -N 1
+## Request the number of CPU cores (There are 24 cores on the GPU node, so 6 cores for 1 GPU)
+#SBATCH -n 6
+#SBATCH -o accelerate_test/test_result.txt
+source savsnet_hpc/bin/activate
+python3 test_classify.py
+## accelerate launch --mixed_precision=fp16 test_classify.py
+
